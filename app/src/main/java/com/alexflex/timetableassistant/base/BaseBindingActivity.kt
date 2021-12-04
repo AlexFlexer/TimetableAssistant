988ff1f1568ff151f586f60641183c69ac96a7f4
Александр Дяченко <a.dyachenko@tirscript.com>
2021-12-04 19:58:57 +0200

    BaseBindingActivity.kt: avoid memory leak while working with ViewBinding

---
+++ app/src/main/java/com/alexflex/timetableassistant/base/BaseBindingActivity.kt
@@ -0,0 +1,23 @@
+package com.alexflex.timetableassistant.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseBindingActivity<VB : ViewBinding> : AppCompatActivity() {
+    private var _binding: VB? = null
+    val binding: VB
+        get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = createBinding()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    abstract fun createBinding(): VB
+}
