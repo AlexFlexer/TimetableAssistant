6f1531c1023f96cff59d97da15866fa355348ace
Александр Дяченко <a.dyachenko@tirscript.com>
2021-12-04 20:17:46 +0200

    SplashActivity.kt and activity_splash.xml: splash screen containing brand icon and text

---
+++ app/src/main/java/com/alexflex/timetableassistant/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.alexflex.timetableassistant
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.alexflex.timetableassistant.base.BaseBindingActivity
+import com.alexflex.timetableassistant.databinding.ActivitySplashBinding
+import kotlinx.coroutines.delay
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : BaseBindingActivity<ActivitySplashBinding>() {
+
+    override fun createBinding(): ActivitySplashBinding {
+        return ActivitySplashBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        lifecycleScope.launchWhenResumed {
+            delay(1000L)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        }
+    }
+}
