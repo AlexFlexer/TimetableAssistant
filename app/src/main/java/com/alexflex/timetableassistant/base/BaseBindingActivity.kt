9f6441e4552efe844b7b2bfb06f9a5f8107d1074
Александр Дяченко <a.dyachenko@tirscript.com>
2021-12-05 20:32:39 +0200

    BaseBindingActivity.kt: returned back to backed binding field

--- app/src/main/java/com/alexflex/timetableassistant/base/BaseBindingActivity.kt
+++ app/src/main/java/com/alexflex/timetableassistant/base/BaseBindingActivity.kt
@@ -3,17 +3,20 @@ package com.alexflex.timetableassistant.base
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
-import com.alexflex.timetableassistant.utils.autoDestroyLifecycleComponent
 
 abstract class BaseBindingActivity<VB : ViewBinding> : AppCompatActivity() {
 
-    val binding: VB by autoDestroyLifecycleComponent({
-        createBinding()
-    })
+    private var _binding: VB? = null
+    val binding: VB get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
     abstract fun createBinding(): VB
