63d0280f4942f4589d04df83ce919b71677483f5
Александр Дяченко <a.dyachenko@tirscript.com>
2021-12-05 17:19:13 +0200

    AddTimetableActivity.kt: fabric method to start it

--- app/src/main/java/com/alexflex/timetableassistant/ui/main/addtimetable/AddTimetableActivity.kt
+++ app/src/main/java/com/alexflex/timetableassistant/ui/main/addtimetable/AddTimetableActivity.kt
@@ -1,10 +1,32 @@
 package com.alexflex.timetableassistant.ui.main.addtimetable
 
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import com.alexflex.timetableassistant.R
 import com.alexflex.timetableassistant.base.BaseBindingActivity
 import com.alexflex.timetableassistant.databinding.ActivityAddTimetableBinding
+import com.alexflex.timetableassistant.utils.createBundleAndPut
+import com.alexflex.timetableassistant.utils.extra
 
 class AddTimetableActivity : BaseBindingActivity<ActivityAddTimetableBinding>() {
+
+    companion object {
+        fun startForNamedTimetable(context: Context, timetableName: String) {
+            context.startActivity(Intent(context, AddTimetableActivity::class.java).also {
+                createBundleAndPut(AddTimetableActivity::mTimetableName to timetableName)
+            })
+        }
+    }
+
+    val mTimetableName: String by extra()
+
     override fun createBinding(): ActivityAddTimetableBinding {
         return ActivityAddTimetableBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.toolbar.text = getString(R.string.main_add_timetable_title, mTimetableName)
     }
 }
