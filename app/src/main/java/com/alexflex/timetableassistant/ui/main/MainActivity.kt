e944724a26abed029ce3d4534a345c092b669264
Александр Дяченко <a.dyachenko@tirscript.com>
2021-12-05 16:43:01 +0200

    activity_main.xml and MainActivity.kt: removed filter

--- app/src/main/java/com/alexflex/timetableassistant/ui/main/MainActivity.kt
+++ app/src/main/java/com/alexflex/timetableassistant/ui/main/MainActivity.kt
@@ -8,24 +8,14 @@ import com.alexflex.timetableassistant.ui.main.addtimetable.AddTimetableActivity
 
 class MainActivity : BaseBindingActivity<ActivityMainBinding>() {
 
-    companion object {
-        @JvmStatic
-        val TAG = MainActivity::class.java.name
-    }
-
     override fun createBinding(): ActivityMainBinding {
         return ActivityMainBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding.apply {
-            btnAdd.setOnClickListener {
-                startActivity(Intent(this@MainActivity, AddTimetableActivity::class.java))
-            }
-            btnFilter.setOnClickListener {
-                FilterDialogFragment().show(supportFragmentManager, TAG)
-            }
+        binding.btnAdd.setOnClickListener {
+            startActivity(Intent(this@MainActivity, AddTimetableActivity::class.java))
         }
     }
 }
