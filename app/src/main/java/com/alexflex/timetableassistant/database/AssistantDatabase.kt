9c3663af564ba19d644212b157ac8c336cf5cf6b
Александр Дяченко <a.dyachenko@tirscript.com>
2021-12-05 18:42:11 +0200

    AssistantDatabase.kt: database setup

---
+++ app/src/main/java/com/alexflex/timetableassistant/database/AssistantDatabase.kt
@@ -0,0 +1,60 @@
+package com.alexflex.timetableassistant.database
+
+import androidx.room.*
+import com.alexflex.timetableassistant.utils.parseType
+import com.google.gson.Gson
+import kotlinx.coroutines.flow.Flow
+import org.koin.java.KoinJavaComponent.inject
+
+const val TIMETABLE_TABLE = "tt_table"
+
+// this class represents item in the timetable
+data class TimetableItem(
+    val index: Int,
+    val name: String,
+    val description: String,
+    val auditory: String
+)
+
+data class WeekDayTimetable(
+    val weekdayIndex: Int,
+    val lectures: TimetableItem
+)
+
+@Entity(tableName = TIMETABLE_TABLE)
+class TimetableEntity(
+    val lessons: List<WeekDayTimetable>,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
+)
+
+@Dao
+interface TimetableDao {
+    @Query("select * from tt_table")
+    suspend fun getAllTimetables(): Flow<TimetableEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun putTimetableEntity(entity: TimetableEntity)
+}
+
+@Database(entities = [TimetableEntity::class], version = 1)
+@TypeConverters(WeekDayTimetableConverter::class)
+abstract class TimetableDatabase : RoomDatabase() {
+    abstract fun timetableDao(): TimetableDao
+}
+
+class WeekDayTimetableConverter {
+
+    @TypeConverter
+    fun fromWeekdayTimetable(list: List<WeekDayTimetable>): String {
+        if (list.isEmpty()) return ""
+        val gson by inject<Gson>(Gson::class.java)
+        return gson.toJson(list)
+    }
+
+    @TypeConverter
+    fun toWeekdayTimetable(json: String?): List<WeekDayTimetable> {
+        if (json.isNullOrBlank()) return emptyList()
+        val gson by inject<Gson>(Gson::class.java)
+        return json.parseType(gson) ?: emptyList()
+    }
+}
